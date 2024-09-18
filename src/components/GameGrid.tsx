5d76777ef9da20b6399f17e0a457b097a7f004ae
Daniel Lanao <daniel.lanao.s@gmail.com>
2024-09-18 16:33:31 -0500

    Filter Games by Genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,23 +3,28 @@ import useGames from "../hooks/useGames"
 import GameCard from "./GameCard";
 import { GameCardSkeleton } from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenres";
 
-const GameGrid = () => {    
-    const{data,error,isLoading}=useGames();
+interface Props{
+    selectedGenre: Genre | null;
+}
+
+const GameGrid = ({selectedGenre}:Props) => {    
+    const{data,error,isLoading}=useGames(selectedGenre);
     const skeletons = [1,2,3,4,5,6];
 
     return (
         <>
         {error && <p>{error}</p>}
-        <SimpleGrid columns={{ sm:1,md:2,lg:3,xl:5 }} spacing={3} padding={10}>
+        <SimpleGrid columns={{ sm:1,md:2,lg:3,xl:5 }} spacing={3} padding={3}>
             {isLoading && skeletons.map((skeleton)=>
             <GameCardContainer key={skeleton}>
-                <GameCardSkeleton key={skeleton}/>
+                <GameCardSkeleton/>
             </GameCardContainer>
             )}
             {data.map((game)=>(
                 <GameCardContainer key={game.id}>
-                    <GameCard key={game.id} game={game}/>        
+                    <GameCard game={game}/>        
                 </GameCardContainer>
             ))}            
         </SimpleGrid>               
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from "react"
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FecthResponse<T>
 {
@@ -9,7 +9,7 @@ interface FecthResponse<T>
     results: T[];
 }
 
-const useData = <T>(endpoint:string) => {
+const useData = <T>(endpoint:string, requestConfig?:AxiosRequestConfig,deps?:any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState('');
     const [isLoading, setLoading] = useState(false);   
@@ -19,7 +19,7 @@ const useData = <T>(endpoint:string) => {
         const controller = new AbortController();
         setLoading(true);
 
-        apiClient.get<FecthResponse<T>>(endpoint, {signal:controller.signal})
+        apiClient.get<FecthResponse<T>>(endpoint, {signal:controller.signal,...requestConfig})
         .then((res)=>{
             setData(res.data.results);
             setLoading(false);
@@ -30,7 +30,7 @@ const useData = <T>(endpoint:string) => {
             setLoading(false);
         });
         return () => controller.abort();
-    },[]);
+    },deps?[...deps]:[]);
 
     return {data, error, isLoading}
 }
