9650c280755b3b935297e9f80f42d85d136a008f
Daniel Lanao <daniel.lanao.s@gmail.com>
2024-09-21 23:13:54 -0500

    Clean up , genres static

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import getCroppedImageUrl from '../services/image-url'
+import Emoji from './Emoji'
 
 interface Props
 {
@@ -14,12 +15,13 @@ const GameCard = ({game}:Props) => {
   return (
     <Card>
         <Image src={getCroppedImageUrl(game.background_image)}></Image>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            <HStack justifyContent='space-between'>
+        <CardBody>            
+            <HStack justifyContent='space-between' marginBottom={3}>
                 <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
                 <CriticScore score={game.metacritic}/>
             </HStack>
+            <Heading fontSize='2xl'>{game.name} </Heading>
+            <Emoji rating={game.rating_top}></Emoji>            
         </CardBody>
     </Card>
   )
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { Button, Heading, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
@@ -14,15 +14,17 @@ const GenreList = ({selectedGenre, onSelectGenre}:Props) => {
   return (
     <>
     {error && <p>{error}</p>}
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
     {data.map((genre)=>(
         <ListItem key={genre.id} paddingY='5px'>            
             <HStack>
                 <Image boxSize='32px' 
                 borderRadius={8} 
+                objectFit='cover'
                 src={getCroppedImageUrl(genre.image_background)}>                    
                 </Image>
-                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} 
+                <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} 
                 onClick={()=>onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
             </HStack>
         </ListItem>
